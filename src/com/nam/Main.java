eacf8e6add9b30899e1cfcacf0725a4debd94cc2
hp <namstudionsk@gmail.com>
2018-01-28 21:40:18 +0700

    Add all logback files

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -1,8 +1,63 @@
 package com.nam;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.Thread.sleep;
+
 public class Main {
+
+    final static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 	// write your code here
+
+        logger.info("Info");
+
+        logger.debug("Debug");
+
+        logger.warn("Warn");
+
+        logger.error("Error");
+
+        wtf();
+
+    }
+
+    private static void wtf()
+    {
+        logger.error("Wtf");
+
+        MMM mmm = new MMM();
+
+        mmm.MMM();
+    }
+}
+
+class MMM
+{
+    final static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    void MMM()
+    {
+        logger.debug("MMM");
+
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                //do {
+                    logger.debug("Run!!");
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                //} while (true);
+            }
+        });
+
+        th.start();
     }
 }
